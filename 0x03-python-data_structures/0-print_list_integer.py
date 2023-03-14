e91ab449012d8449364049ffbc4dbba0f327a5ce
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-13 17:33:21 -0700

    Fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,4 @@
 def print_list_integer(my_list=[]):
 
     for integer in my_list:
-        print("{}".format(integer))
-
-# my_list = [1, 2, 3, 4, 5]
-# print_list_integer(my_list)
+        print("{0}".format(integer))
