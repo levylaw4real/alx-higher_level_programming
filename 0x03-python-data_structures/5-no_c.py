d18655875d1b6cec5329f5421e32699f0f8afc7d
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-13 18:23:46 -0700

    test fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,3 @@ def no_c(my_string):
     new_string = my_string.translate({ord(i): None for i in 'cC'})
     # new_string = "".join([char for char in my_string if char not in 'cC'])
     return new_string
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
