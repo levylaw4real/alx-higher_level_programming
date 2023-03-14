88f201d486b41d10b4c45a64054bd8e39ff4d109
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-13 18:25:17 -0700

    test fix

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -2,5 +2,4 @@
 a = 89
 b = 10
 a, b = b, a
-# tuple unpacking to swap the values of a and b in one line:
-print("a={:d} - b={:d}".format(a, b))
+print("a={:d} - b={:d}".format(a, b)) # tuple unpacking to swap the values of a and b in one line
