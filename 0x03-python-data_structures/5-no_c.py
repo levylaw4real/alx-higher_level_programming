ad995f0093a5f1411d189783c462235487300d30
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-13 18:22:35 -0700

    test fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # new_string = my_string.translate({ord(i): None for i in 'cC'})
-    new_string = "".join([char for char in my_string if char not in 'cC'])
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    # new_string = "".join([char for char in my_string if char not in 'cC'])
     return new_string
 
 print(no_c("Best School"))
