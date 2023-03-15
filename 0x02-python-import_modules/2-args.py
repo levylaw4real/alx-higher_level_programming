52f7b78671b8125db7ba8177bf83a10b753f6926
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-15 11:50:13 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = sys.argv[1:] # exclude script name
-num_args = len(args)
+    args = sys.argv[1:] # exclude script name
+    num_args = len(args)
 
-if num_args == 0:
-    print("0 arguments.")
-else:
-    if num_args == 1:
-        arg_str = "argument:"
+    if num_args == 0:
+        print("0 arguments.")
     else:
-        arg_str = "arguments:"
-    print("{} {}{}".format(num_args, arg_str, "" if num_args == 0 else ":"))
+        if num_args == 1:
+            arg_str = "argument:"
+        else:
+            arg_str = "arguments:"
+        print("{} {}{}".format(num_args, arg_str, "" if num_args == 0 else ":"))
 
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
