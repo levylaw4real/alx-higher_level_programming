ea1a196389c8e04b654e2935f64c6545cf1a5802
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-15 11:51:02 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
-    args = sys.argv[1:] # exclude script name
-    num_args = len(args)
-
-    if num_args == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        if num_args == 1:
-            arg_str = "argument:"
-        else:
-            arg_str = "arguments:"
-        print("{} {}{}".format(num_args, arg_str, "" if num_args == 0 else ":"))
-
-        for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
