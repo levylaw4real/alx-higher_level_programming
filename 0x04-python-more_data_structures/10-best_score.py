27d3bd81cb2bde8e0154445bb6e558e599750bad
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-15 20:05:42 -0700

    fix 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if not my_dict:
-        return None
-    max_value = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value == max_value:
-            return key
+	if not my_dict:
+		return None
+	max_value = max(my_dict.values())
+	for key, value in my_dict.items():
+		if value == max_value:
+			return key
 
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    return {k: v*2 for k, v in my_dict.copy().items()}
+	return {k: v*2 for k, v in my_dict.copy().items()}
 
